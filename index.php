ff6b5f2c9d2f8f8a6a1b3d179b99d855dc5b4293
CarstenLubetzki <35264229+CarstenLubetzki@users.noreply.github.com>
2018-01-11 19:49:19 +0100

    Update index.php
    create atoms for img and headlines

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="source/00-atoms/images/16x9/16x9.css"/>
+    <link rel="stylesheet" href="source/00-atoms/headlines/h1/h1.css"/>
+</head>
+<body>
+<?php
+include "source/00-atoms/headlines/h1/h1.php";
+include "source/00-atoms/headlines/h1/h1.php";
+include "source/00-atoms/images/16x9/16x9.php";
+
+
+?>
+
+
+</body>
+</html>
---
+++ source/00-atoms/images/16x9/16x9.php
@@ -0,0 +1,5 @@
+<img class="image16x9"
+     src="https://dummyimage.com/1600x900/000/fff"
+     alt="16x9"
+     title="16 zu 9"
+/>

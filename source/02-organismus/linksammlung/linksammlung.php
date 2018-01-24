873e78d0e502feaffee335ea5cfa893dd6c247a4
CarstenLubetzki <35264229+CarstenLubetzki@users.noreply.github.com>
2018-01-24 18:09:16 +0100

    update version
    little changes

--- source/02-organismus/linksammlung/linksammlung.php
+++ source/02-organismus/linksammlung/linksammlung.php
@@ -1,9 +1,13 @@
 <div class="h1_distances"><?php include "source/00-atoms/headlines/h1/h1.php"; ?></div>
-<div class="linksammlung">
+<ul class="linksammlung">
     <?php
         $nrIncludes = 10;
         for ($i=1;$i<$nrIncludes;$i++) {
-            include "source/00-atoms/link/link.php";
+    ?>
+            <li>
+            <?php include "source/00-atoms/link/link.php"; ?>
+            </li>
+    <?php
         }
     ?>
-</div>
+</ul>
